26dca533cbfe57c4769bd47e93c2e4cf38142768
Marcelo Tisot <tisotmarcelo@gmail.com>
2018-07-11 02:08:48 -0300

    Initial commit

---
+++ src/api/config.js
@@ -0,0 +1,11 @@
+export default {
+	appname: 'platzimusic',
+	apiKey: '6d35d7fe3df36d832af41146d6b48fc7',
+	secret: '04217c1a2ba76791d735fb1d33eb5ca9',
+	registeredTo: 'tisotmarcelo'
+}
+
+// Application name 	platzimusic
+// API key 	6d35d7fe3df36d832af41146d6b48fc7
+// Shared secret 	04217c1a2ba76791d735fb1d33eb5ca9
+// Registered to 	tisotmarcelo
---
+++ src/components/Artist.vue
@@ -0,0 +1,21 @@
+<template lang="pug">
+  li.artist
+    h2: a(:href="artist.url", target="_blank") {{ artist.name }}
+      
+
+    //- img(src="{{}}") NO !
+    img(:src="artist.image[2]['#text']")
+</template>
+
+<script>
+export default {
+  name: 'artist',
+  props: [ 'artist' ]
+}
+</script>
+
+<style lang="stylus" scoped>
+li.artist
+  display block
+  margin 10px 0
+</style>
